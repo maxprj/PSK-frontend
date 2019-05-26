8d7d0a9108b7882e2a7fa25dfeb0e6600e3a516b
IgnasSeskauskas <31855242+IgnasSeskauskas@users.noreply.github.com>
2019-05-26 17:40:02 +0300

    created layout and fixed routing

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,14 +1,19 @@
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './authentication/login';
-import {HomeComponent} from './home/home.component';
 import {AuthenticationGuard} from './authentication/authentication.guard';
 import { PasswordChangeComponent } from './authentication/password-change/password-change.component';
+import { NavComponent } from './core/layout-components/nav/nav.component';
 
 const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthenticationGuard] },
+  { path: '', component: NavComponent, canActivate: [AuthenticationGuard],
+    children: [
+      { path: 'users', loadChildren: 'src/app/users/users.module#UsersModule' },
+      { path: 'apartments', loadChildren: 'src/app/apartments/apartments.module#ApartmentsModule' }
+  ]},
   { path: 'login', component: LoginComponent },
   { path: 'user/changePassword', component: PasswordChangeComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: '', canActivate: [AuthenticationGuard]},
+ 
 ];
 
 export const routing = RouterModule.forRoot(appRoutes);
