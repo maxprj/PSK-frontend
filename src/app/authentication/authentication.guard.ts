66d4f54e89a0321a396f511c5f57bad76a2bcd96
IgnasSeskauskas <31855242+IgnasSeskauskas@users.noreply.github.com>
2019-05-30 21:47:21 +0300

    deleted error-page and footer. Fixed role guard

--- src/app/authentication/authentication.guard.ts
+++ src/app/authentication/authentication.guard.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
 import {TOKEN_PSK} from '../utils/constants';
-import {AuthenticationService} from './authentication.service';
+import { AuthenticationService } from './authentication.service';
+import { DefaultRootService } from './default-root.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,15 @@ import {AuthenticationService} from './authentication.service';
 export class AuthenticationGuard implements CanActivate {
   constructor(
     private router: Router,
-    private authenticationService: AuthenticationService
+    private authenticationService: AuthenticationService,
+    private defaultRootService: DefaultRootService
   ) {}
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (localStorage.getItem(TOKEN_PSK) != null) {
       const userRole = this.authenticationService.currentUserRole;
       if (next.data.roles && next.data.roles.indexOf(userRole) === -1) {
-        this.router.navigate(['/error']);
+        this.router.navigate([this.defaultRootService.getHomePageBaseOnUserRole]);
         return false;
       }
 
